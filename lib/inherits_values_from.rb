7ce7e001557ea4ffe340c083044f3434edd58751
Colin Burn-Murdoch <colin@burn-murdoch.com>
2016-03-08 22:47:26 +0000

    :Write value as nil if it matches inherited value.

--- lib/inherits_values_from.rb
+++ lib/inherits_values_from.rb
@@ -1,26 +1,45 @@
 require 'inherits_values_from/version.rb'
 
 module InheritsValuesFrom
-  
+
   extend ActiveSupport::Concern
-  
+
   module ClassMethods
-    
+
     def inherits_values_from(model, values)
       values.each do |local, far|
         far = local if far.nil? # It's an array
+
         define_method(local) do
           if self[local].nil?
-            self.method(model).call[far]
+            self.method(model).call().method(far).call()
           else
             self[local]
           end
         end
+
+
+        # # If a value is set explicitly to the same as the inherited value
+        # # should we set this as null (inherit), or keep it specified?
+        # #
+        # define_method("#{local}=") do |value|
+        #   if self.method(model).call[far] === value
+        #     write_attribute(local, nil)
+        #   else
+        #     write_attribute(local, value)
+        #   end
+        # end
+
+
+        define_method("#{local}_inherited") do
+          self.method(model).call[far]
+        end
+
       end
     end
-    
+
   end
-  
+
 end
 
 ActiveRecord::Base.send(:include, InheritsValuesFrom)
